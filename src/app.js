85c93973c7ff71bd39b995d076699f33c0f9ec37
Abhishek <2020pgcais14@nitjsr.ac.in>
2025-08-09 20:58:09 +0530

    basic get post api

--- src/app.js
+++ src/app.js
@@ -3,12 +3,27 @@ console.log("Starting a new project");
 const express = require("express");
 const app =express();
 
-app.use("/hello",(req,res)=>{
+//This will match all the GET method api calls to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Abhishek", lastName:"Kumar"})
+
+});
+app.post("/user",(req,res)=>{
+    res.send("Data successfully saved to Database")
+})
+
+
+//This will match all the HTTP method API calls to /test
+app.use("/test",(req,res)=>{
     res.send("Hello from Server");
 });
-app.use("/dashboard",(req,res)=>{
-    res.send("Hello from dashboard")
-});
+// app.use("/dashboard",(req,res)=>{
+//     res.send("Hello from dashboard")
+// });
+// app.use("/",(req,res)=>{
+
+//     res.send("Hello Mr. Abhishek")
+// });
 app.listen(3000,()=>{
     console.log("Server is succesfully listening on port 3000.......");
 });
